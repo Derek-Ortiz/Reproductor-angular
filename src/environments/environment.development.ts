1729c8c5d9856fa50412dd486ab29205156ed90d
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2025-11-18 09:26:50 -0600

    push de environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    API_URL: "https://api.spotify.com/v1/",
+    AUTH_API_URL: "https://accounts.spotify.com/api/token",
+    CLIENT_ID : "f13f8542115d4746a4e22e4fa50feadd",
+    CLIENT_SECRET : "a928af97de694e14a6b03e7f563f3a31"
+};
